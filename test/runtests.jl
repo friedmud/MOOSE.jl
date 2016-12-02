a160996d36e6f7ac3f9f1f32f13c70a8e55cf356
Derek Gaston <friedmud@gmail.com>
2016-12-02 15:13:34 -0500

    Turn tests back on

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,23 +7,23 @@ using ForwardDiff: Partials, Dual, value, partials, npartials, setindex
 
 using Base.Test
 
-#include("test_Node.jl")
-#include("test_Element.jl")
-#include("test_Generation.jl")
-#include("test_Variable.jl")
-#include("test_System.jl")
-#include("test_Solver.jl")
-#include("test_Kernel.jl")
-#include("test_Assembly.jl")
-#
-#include("test_Diffusion.jl")
-#
-#include("test_DirichletBC.jl")
-#
-#include("test_VTKOutput.jl")
-#
-#include("test_JuliaDenseImplicitSolver.jl")
-#
-#include("test_PetscImplicitSolver.jl")
+include("test_Node.jl")
+include("test_Element.jl")
+include("test_Generation.jl")
+include("test_Variable.jl")
+include("test_System.jl")
+include("test_Solver.jl")
+include("test_Kernel.jl")
+include("test_Assembly.jl")
+
+include("test_Diffusion.jl")
+
+include("test_DirichletBC.jl")
+
+include("test_VTKOutput.jl")
+
+include("test_JuliaDenseImplicitSolver.jl")
+
+include("test_PetscImplicitSolver.jl")
 
 include("test_Mesh.jl")
